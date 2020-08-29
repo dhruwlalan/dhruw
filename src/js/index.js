649c53b85aef953041fc3d61b7e531f311fc34d5
dhruw <dhruwlalan22@gmail.com>
2020-08-29 23:10:19 +0530

    Deleted Old Code

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,8 @@
 import './assets.js';
 
 // MAIN
+
+
+$(window).on("scroll", function() {
+    $(window).scrollTop() >= 50 ? $(".header-navbar").addClass("header-navbar--sticky") : $(".header-navbar").removeClass("header-navbar--sticky")
+})
--- src/js/script.js
+++
@@ -1,4 +0,0 @@
-
-$(window).on("scroll", function() {
-    $(window).scrollTop() >= 50 ? $(".header-navbar").addClass("header-navbar--sticky") : $(".header-navbar").removeClass("header-navbar--sticky")
-})
