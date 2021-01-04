587c6cdf74cbfa7b9d554556743f90163df59975
dhruw <dhruwlalan22@gmail.com>
2021-01-04 16:15:48 +0530

    Initial build

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
    <div class="container">
-      <h1>Hello, World!!</h1>
-      <button @click="this.count++">count: {{ count }}</button>
+      <h1>Hello,</h1>
+      <h1>My name is dhruw lalan.</h1>
+      <br />
+      <h4>I love building cool stuff on the web!</h4>
+      <h4></h4>
    </div>
 </template>
 
-<script>
-export default {
-   data() {
-      return {
-         count: 0,
-      };
-   },
-};
-</script>
+<style>
+h1 {
+   color: #95d4ff;
+   font-family: var(--courier);
+   font-size: 4rem;
+   font-weight: 700;
+}
+h4 {
+   color: #7b93a4;
+   font-family: var(--courier);
+   font-size: 2.4rem;
+   font-weight: 400;
+}
+</style>
