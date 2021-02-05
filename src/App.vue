1bdf3a0ec7b501848b979627589c9018d4320c79
dhruw <dhruwlalan22@gmail.com>
2021-02-05 10:34:47 +0530

    added future goal

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,32 @@
    <div class="container">
       <h1>Hello,</h1>
       <h1>My name is dhruw lalan.</h1>
-      <br />
-      <h4>I love building cool stuff on the web!</h4>
-      <h4></h4>
+      <h4 class="bio">I love building cool stuff on the web!</h4>
+      <h5 class="bio">Future Goals: Contribute to Open Source.</h5>
    </div>
 </template>
 
 <style>
 h1 {
-   color: #95d4ff;
+   color: #30bcff;
    font-family: var(--courier);
    font-size: 4rem;
    font-weight: 700;
+   margin-bottom: 1rem;
 }
 h4 {
-   color: #7b93a4;
+   color: #9ec2dc;
    font-family: var(--courier);
    font-size: 2.4rem;
    font-weight: 400;
+   margin-top: 2rem;
+}
+h5 {
+   color: #9ec2dc;
+   font-family: var(--courier);
+   font-size: 2rem;
+   font-weight: 400;
+   font-style: italic;
+   margin-top: 2rem;
 }
 </style>
