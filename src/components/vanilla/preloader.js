bddc6f4d2be47c3e1aa174982de115d155c72c61
dhruw <dhruwlalan22@gmail.com>
2021-02-25 13:51:45 +0530

    chore: add workspace settings

--- src/components/vanilla/preloader.js
+++ src/components/vanilla/preloader.js
@@ -2,8 +2,8 @@ const preloader = document.querySelector('.preloader');
 
 window.addEventListener('load', () => {
    setTimeout(() => {
-      // preloader.classList.add('move-down');
-      preloader.classList.add('fade-out');
+      preloader.classList.add('move-down');
+      // preloader.classList.add('fade-out');
       setTimeout(() => {
          preloader.remove();
       }, 1000);
